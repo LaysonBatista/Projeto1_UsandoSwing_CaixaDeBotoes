70ca8334f7324ca2369d55054be25db359b878cb
LaysonBatista <117116524+LaysonBatista@users.noreply.github.com>
2023-02-13 21:39:49 -0300

    Add files via upload

---
+++ Projeto2_UsandoSwing_CaixaDeBotoes.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto2_usandoswing_CaixaDeBotoes;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author aluno
+ */
+public class Projeto2_UsandoSwing_CaixaDeBotoes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i;
+        
+        JFrame janela = new JFrame("Titulo da janela");
+        janela.setBounds(50,100,400,150);
+        janela.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        
+        FlowLayout flow = new FlowLayout();
+        Container caixa = janela.getContentPane();
+        caixa.setLayout(flow);
+        for(i=1; i<=6; i++){
+            caixa.add(new JButton("Aperta " + i));
+            janela.setVisible(true);
+        }
+        
+        
+        
+    }
+    
+}
